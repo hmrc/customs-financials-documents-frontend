46e5917b215fa92e26ca4c06d6413fa583c61804
zach.hall <29452017+zachhall1234@users.noreply.github.com>
2022-06-01 11:42:52 +0100

    play upgrade 3.21.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "5.16.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc" % "1.27.0-play-28"
+    "uk.gov.hmrc" %% "play-frontend-hmrc" % "3.21.0-play-28"
   )
 
   val test = Seq(
