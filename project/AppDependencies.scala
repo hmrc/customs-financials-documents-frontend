89a2df636fdd8d73bd555d58a09866e93a05a0d3
wg-hmrc <107496826+wg-hmrc@users.noreply.github.com>
2022-10-24 08:50:21 +0100

    Dependency update (#24)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "5.24.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc" % "3.21.0-play-28"
+    "uk.gov.hmrc" %% "play-frontend-hmrc" % "3.32.0-play-28"
   )
 
   val test = Seq(
