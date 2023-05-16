0b1a72235a15ad408a42b436b1a91b03cc582996
bsgangapat <106598990+bsgangapat@users.noreply.github.com>
2023-05-16 15:11:47 +0100

    MMCA-4000 update latest version plugins

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "7.15.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc" % "7.3.0-play-28"
+    "uk.gov.hmrc" %% "play-frontend-hmrc" % "7.7.0-play-28"
   )
 
   val test = Seq(
