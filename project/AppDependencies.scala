5869992fbb7383d337554357709ed4f124e70797
sameerhmrc <106383455+sameerhmrc@users.noreply.github.com>
2023-10-02 11:32:00 +0100

    MMCA-4258 : Update version & configuration for CDS Financials service… (#64)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,11 +2,11 @@ import sbt._
 
 object AppDependencies {
 
-  val bootstrapVersion = "7.19.0"
+  val bootstrapVersion = "7.22.0"
 
   val compile = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootstrapVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc" % "7.14.0-play-28"
+    "uk.gov.hmrc" %% "play-frontend-hmrc" % "7.21.0-play-28"
   )
 
   val test = Seq(
@@ -14,6 +14,7 @@ object AppDependencies {
     "org.jsoup" % "jsoup" % "1.16.1" % Test,
     "com.vladsch.flexmark" % "flexmark-all" % "0.36.8" % Test,
     "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0" % Test,
-    "org.mockito" %% "mockito-scala-scalatest" % "1.17.14" % Test
+    "org.mockito" %% "mockito-scala-scalatest" % "1.17.14" % Test,
+    "uk.gov.hmrc" %% "play-frontend-hmrc" % "7.21.0-play-28"
   )
 }
