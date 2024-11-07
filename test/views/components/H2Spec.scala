4bdd0a4a8f9bb188163e3937dd4ccbfd3d7404b6
Ned Sakalauskis <62673820+Neozxc@users.noreply.github.com>
2024-11-07 23:40:39 +0000

    MMCA-4809 | Add missing component unit tests

---
+++ test/views/components/H2Spec.scala
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.components
+
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.select.Elements
+import org.scalatest.matchers.must.Matchers.convertToAnyMustWrapper
+import play.api.Application
+import play.api.i18n.Messages
+import utils.SpecBase
+import views.html.components.h2
+
+class H2Spec extends SpecBase {
+
+  "h2 component" should {
+
+    "display correct contents" when {
+
+      "only message key has been provided" in new Setup {
+        h2Component.text() mustBe message(msgKey)
+
+        val h2: Elements = h2Component.select("h2")
+        h2.size() mustBe 1
+        h2.first().hasAttr("class") mustBe true
+        h2.first().classNames() must contain(defaultClass)
+        h2.first().hasAttr("id") mustBe false
+      }
+
+      "class has been provided along with message key" in new Setup {
+        h2ComponentWithClass.text() mustBe message(msgKey)
+
+        val h2: Elements = h2ComponentWithClass.select("h2")
+        h2.first().classNames() must contain(classes)
+      }
+
+      "id has been provided along with message key" in new Setup {
+        h2ComponentWithId.text() mustBe message(msgKey)
+
+        val h2: Elements = h2ComponentWithId.select("h2")
+        h2.first().attr("id") mustBe id
+      }
+
+      "classes and id have been provided along with message key" in new Setup {
+        h2ComponentWithClassAndId.text() mustBe message(msgKey)
+
+        val h2: Elements = h2ComponentWithClassAndId.select("h2")
+        h2.first().classNames() must contain(classes)
+        h2.first().attr("id") mustBe id
+      }
+    }
+  }
+
+  trait Setup {
+    val msgKey = "messageKey"
+    val defaultClass = "govuk-heading-m"
+    val classes = "custom-class"
+    val id = "custom-id"
+
+    val app: Application = application().build()
+    implicit val message: Messages = messages(app)
+    val instanceOfH2: h2 = app.injector.instanceOf[h2]
+
+    val h2Component: Document = Jsoup.parse(instanceOfH2(msgKey).body)
+    val h2ComponentWithClass: Document = Jsoup.parse(instanceOfH2(msgKey, classes = classes).body)
+    val h2ComponentWithId: Document = Jsoup.parse(instanceOfH2(msgKey, id = Some(id)).body)
+    val h2ComponentWithClassAndId: Document = Jsoup.parse(instanceOfH2(msgKey, id = Some(id), classes = classes).body)
+  }
+}
---
+++ test/views/components/LinkSpec.scala
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.components
+
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.select.Elements
+import org.scalatest.matchers.must.Matchers.convertToAnyMustWrapper
+import play.api.Application
+import play.api.i18n.Messages
+import utils.SpecBase
+import views.html.components.link
+
+class LinkSpec extends SpecBase {
+
+  "link component" should {
+
+    "display correct contents" when {
+
+      "only link message and location have been provided" in new Setup {
+        linkComponent.text() mustBe message(linkMessage)
+
+        val link: Elements = linkComponent.select("a")
+        link.size() mustBe 1
+        link.first().hasAttr("class") mustBe true
+        link.first().classNames() must contain(defaultClass)
+        link.first().attr("href") mustBe location
+        link.first().hasAttr("id") mustBe false
+      }
+
+      "class has been provided along with link message and location" in new Setup {
+        linkComponentWithClass.text() mustBe message(linkMessage)
+
+        val link: Elements = linkComponentWithClass.select("a")
+        link.first().classNames() must contain(classes)
+      }
+
+      "id has been provided along with link message and location" in new Setup {
+        linkComponentWithId.text() mustBe message(linkMessage)
+
+        val link: Elements = linkComponentWithId.select("a")
+        link.first().attr("id") mustBe linkId
+      }
+
+      "classes and id have been provided along with link message and location" in new Setup {
+        linkComponentWithClassAndId.text() mustBe message(linkMessage)
+
+        val link: Elements = linkComponentWithClassAndId.select("a")
+        link.first().classNames() must contain(classes)
+        link.first().attr("id") mustBe linkId
+      }
+
+      "aria-label has been provided along with link message and location" in new Setup {
+        linkComponentWithAriaLabel.text() must include(message(linkMessage))
+        linkComponentWithAriaLabel.text() must include(ariaLabelText)
+
+        val link: Elements = linkComponentWithAriaLabel.select("a")
+        link.first().select(".govuk-visually-hidden").text() mustBe ariaLabelText
+      }
+    }
+  }
+
+  trait Setup {
+    val linkMessage = "linkMessage"
+    val location = "jackie-chan.com"
+    val defaultClass = "govuk-link"
+    val classes = "custom-class"
+    val linkId = "custom-id"
+    val ariaLabelText = "visually hidden label"
+
+    val app: Application = application().build()
+    implicit val message: Messages = messages(app)
+    val instanceOfLink: link = app.injector.instanceOf[link]
+
+    val linkComponent: Document = Jsoup.parse(instanceOfLink(linkMessage, location).body)
+    val linkComponentWithClass: Document = Jsoup.parse(instanceOfLink(linkMessage, location, linkClass = classes).body)
+    val linkComponentWithId: Document = Jsoup.parse(instanceOfLink(linkMessage, location, linkId = Some(linkId)).body)
+
+    val linkComponentWithClassAndId: Document = Jsoup.parse(
+      instanceOfLink(linkMessage, location, linkId = Some(linkId), linkClass = classes).body)
+    val linkComponentWithAriaLabel: Document = Jsoup.parse(
+      instanceOfLink(linkMessage, location, ariaLabel = Some(ariaLabelText)).body)
+  }
+}
