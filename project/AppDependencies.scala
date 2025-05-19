16a43b9cd32751757334653ecdb0140bd0a9857e
Vineet Tyagi <vineet.tyagi@digital.hmrc.gov.uk>
2025-05-19 14:16:56 +0100

    [MMCA-5316] - Update README.md file (#121)
    * [MMCA-5316] Update file to alogn with latest changes
    
    * [MMCA-5316] Update scala version to a placeholder in  coverage report path
    
    * [MMCA-5316] Replace Application to Microservice
    
    * [MMCA-5316] Update library version and remove the redundant libraries
    
    * [MMCA-5316] Incorporate review comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,14 +7,12 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "12.0.0"
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "12.1.0"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc" %% "bootstrap-test-play-30" % bootstrapVersion % Test,
-    "org.jsoup" % "jsoup" % "1.16.1" % Test,
-    "com.vladsch.flexmark" % "flexmark-all" % "0.36.8" % Test,
-    "org.scalatestplus.play" %% "scalatestplus-play" % "7.0.1" % Test,
-    "org.scalatestplus" %% "mockito-4-11" % "3.2.18.0" % Test,
+    "uk.gov.hmrc"       %% "bootstrap-test-play-30" % bootstrapVersion % Test,
+    "org.jsoup"          % "jsoup"                  % "1.16.1"         % Test,
+    "org.scalatestplus" %% "mockito-4-11"           % "3.2.18.0"       % Test
   )
 }
