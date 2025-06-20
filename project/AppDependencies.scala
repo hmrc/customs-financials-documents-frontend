a1123770830aa20af70135a310c1a858a3ac21a4
vineet <vineet.tyagi@digital.hmrc.gov.uk>
2025-06-20 17:33:20 +0100

    [MMCA-5408] Update libraries to address PlatOps warnings

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "12.5.0"
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "12.6.0"
   )
 
   val test: Seq[ModuleID] = Seq(
